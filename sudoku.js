b41fcbc46ef6899a9ef1d33f10345fcb6c771aa1
Toni & Ken Duigraha Putra <pair+chrisantoni.kenduigraha@hacktiv8.com>
2016-09-30 16:51:58 +0700

    sudoku-1

--- sudoku.js
+++ sudoku.js
@@ -1,24 +1,177 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+      this.board_string = board_string
+      this.tampungBoard = []
+      this.cornerRow = -1
+      this.cornerCol = -1
+  }
 
-  solve() {}
+  solve() {
+    //cek baris
+    for(var i = 0 ; i < this.tampungBoard.length; i++){
+      for(var j = 0 ; j < this.tampungBoard.length; j++){
+        if(this.tampungBoard[i][j] === '0'){
+          var temp = [];
+          for(var cek = 9 ; cek > 0 ; cek--){
+            cek = cek.toString();
+            if(this.checkNumRow(i, cek) == true){
+              //nanti cek dimasukin ke angka 0
+              //this.tampungBoard[i][j] = cek
+              // break;//biar ketika nemu ga cari lagi, break for cek kalau cek baris aja
+              //cek kolom
+              if(this.checkNumCol(j, cek) === true){
+                  //this.tampungBoard[i][j] = cek
+                  //temp.push(cek);
+                  //break;
+                  //cek 3x3
+                 if(this.checkNumSquare(i, j, cek) === true){
+                   this.tampungBoard[i][j] = cek
+                 }
+              }
+            }else{
+              //kalau false artinya ga dimasukin
+              //this.tampungBoard[i][j] = 'a'
+            }
+          }
+        }
+      }
+    }
 
-  // Returns a string representing the current state of the board
-  board() {}
+    return this.tampungBoard;
+  }
+
+  substitue(cornerRow, cornerCol,cek)
+  {
+    var flag = 0;
+    cek = cek.toString()
+    for(var i = cornerRow ; i < cornerRow+3 ; i++){
+      for(var j = cornerCol ; j < cornerCol+3 ; j++){
+        if(this.tampungBoard[i][j] === cek){
+          flag++
+        }else{
+          flag += 0
+        }
+      }
+    }
+    if(flag > 0){
+      return false
+    }else{
+      return true
+    }
+  }
+
+  checkNumSquare(row, col, cek){
+    if(row >= 0 && row < 3){
+      if(col >= 0 && col < 3){
+      return this.substitue(0, 0, cek)
+      }else if(col >= 3 && col < 6){
+        return this.substitue(0,3, cek)
+      }else{
+        return this.substitue(0,6, cek)
+      }
+    }else if(row >= 3 && row < 6){
+      if(col >= 0 && col < 3){
+        return this.substitue(3,0, cek)
+      }else if(col >= 3 && col < 6){
+
+        return this.substitue(3,3, cek)
+      }else{
+        return this.substitue(3,6, cek)
+      }
+    }else{
+      if(col >= 0 && col < 3){
+        return this.substitue(6,0, cek)
+      }else if(col >= 3 && col < 6){
+        return this.substitue(6,3, cek)
+      }else{
+        return this.substitue(6,6, cek)
+      }
+    }
+}
+    //console.log("corner : " + cornerRow + " , " + cornerCol)
+
+
+  checkNumCol(col, cek){
+    var flag = 0;
+    for(var row = 0 ; row < 9 ; row++){
+      if(this.tampungBoard[row][col] === cek ){
+        flag++
+      }else{
+        flag += 0
+      }
+    }
+    if(flag > 0){
+      return false
+    }else{
+      return true
+    }
+  }
+
+  checkNumRow(row, cek){
+    var flag = 0;
+    for(var col = 0 ; col < 9 ; col++){
+      if(this.tampungBoard[row][col] === cek ){
+        //console.log(this.tampungBoard[row][col]);
+        flag++
+      }else{
+        flag += 0
+      }
+    }
+    if(flag > 0){
+      return false
+    }else{
+      return true
+    }
+  }
+
+  board() {
+    for(var i=0;i<9;i++)
+    {
+      this.tampungBoard[i] =  []
+      for(var j=0;j<9;j++)
+      {
+        this.tampungBoard[i].push(this.board_string[9*i+j])
+      }
+    }
+    return this.tampungBoard
+  }
+
+  board_random()
+  {
+    for(var i=0;i<this.tampungBoard.length;i++)
+    {
+      for(var j=0;j<this.tampungBoard.length;j++)
+      {
+        if(this.tampungBoard[i][j] === '0')
+        {
+          this.tampungBoard[i][j] = random_number()
+        }
+      }
+    }
+    return this.tampungBoard
+  }
 }
 
-// The file has newlines at the end of each line,
-// so we call split to remove it (\n)
 var fs = require('fs')
+var board_string_test = '609238745274561398853947621486352179792614583531879264945723816328196457167485932'
 var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
   .toString()
   .split("\n")[0]
 
 var game = new Sudoku(board_string)
 
+function random_number()
+{
+  return (Math.floor(Math.random()*9)+1).toString()
+}
+
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
+// game.solve()
 
 console.log(game.board())
+console.log('\n');
+//console.log(game.board_random());
+console.log(game.solve());
+// console.log(game.checkNumSquare(7,6, 8));
